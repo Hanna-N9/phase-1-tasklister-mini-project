d1b64f0cf3f83aaf7ac31c863c87444c6855aae5
Hanna Negash <hnegash.n@gmail.com>
2023-09-23 17:43:31 -0700

    To-do list application using form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    handleToDo(e.target.newtaskdescription.value)
+    form.reset() //empty form
+
+  });
 });
+
+function handleToDo(todo) {
+  let li = document.createElement("li"); //returns new a Node
+  li.textContent = `${todo} `;
+  document.querySelector("#tasks").appendChild(li);
+
+  let btn = document.createElement("button");
+  button.textContent = "x"
+  li.appendChild(btn)
+  btn.addEventListener("click", handleDelete)
+  
+
+} 
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
